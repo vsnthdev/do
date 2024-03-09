6d66a9f3d593dc2aeaadfc601c9041f9fae36ca6
Vasanth Srivatsa <vsnthdev@gmail.com>
2024-03-09 14:51:13 +0530

    🎊 improve clean cmd

--- cmds/clean.ts
+++ cmds/clean.ts
@@ -1,27 +1,63 @@
 import { $ } from 'bnx'
+import { Listr } from 'listr2'
+import { execaCommand } from 'execa'
 import { type Command } from 'commander'
 
+interface Ctx {
+    currentBranch: string
+    remotes: string[]
+}
+
 async function action() {
-    const branch = (await $`git branch --show-current`).trim()
+    const currentBranch = (await $`git branch --show-current`).trim()
+    const remotes = (await $`git remote`).trim().split('\n')
 
-    if (branch == 'main') return console.log('You are already in main!')
+    if (currentBranch == 'main') return console.log('You are already in main!')
+
+    const tasks = new Listr<Ctx>(
+        [
+            {
+                title: 'Switching to main branch',
+                task: () => execaCommand('git checkout main')
+            },
+        ], {
+        ctx: {
+            currentBranch,
+            remotes,
+        }
+    }
+    )
+
+    tasks.add({
+        title: 'Fetching updates',
+        task: (ctx, task): Listr =>
+            task.newListr([
+                {
+                    title: 'Fetching updates',
+                    task: () => execaCommand('git fetch')
+                },
+            ].concat(remotes.map(remote => ({
+                title: `Fetching ${remote} remote`,
+                task: () => execaCommand(`git fetch ${remote}`)
+            }))))
+    })
+
+    tasks.add([
+        {
+            title: `Pulling new changes`,
+            task: () => execaCommand(`git pull`)
+        },
+        {
+            title: `Deleting ${currentBranch} branch`,
+            task: () => execaCommand(`git branch -D ${currentBranch}`)
+        }
+    ])
 
     try {
-        await $`git checkout main`
-    } catch { }
-    try {
-        await $`git fetch`
-    } catch { }
-    try {
-        await $`git fetch upstream`
-    } catch { }
-    try {
-        await $`git pull`
-    } catch { }
-
-    try {
-        await $(`git branch -D "${branch}"`)
-    } catch { }
+        await tasks.run()
+    } catch (e) {
+        console.error(e)
+    }
 }
 
 export default function setup(app: Command) {
